f68ebadc798c44d7973a337db38eeb59e7eb9bbc
Malo Reinert <malo.reinert@hei.yncrea.fr>
2020-04-01 11:45:10 +0200

    fin projet

--- jeu-pendu/src/App.js
+++ jeu-pendu/src/App.js
@@ -4,16 +4,18 @@ import shuffle from 'lodash.shuffle'
 import PropTypes from 'prop-types'
 import NbreEssais from "./NbreEssais";
 
-var tabMots = ["Malo", "Alix", "Bibou", "Tableau", "Fun", "Mistrale"]
+var tabMots = ["MALO", "ALIX", "BIBOU", "TABLEAU", "FUN", "MISTRALE"]
 const LETTRES = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
-class App extends Component{
+
+class App extends Component {
     state = {
         motActuel: this.generatedMot(),
         nbreEssai: 0,
         lettreChoisi: '',
-        lettreUtilise : new Set(),
-        etatMot : this.computeDisplay ,
+        lettreUtilise: new Set(),
+        etatMot: this.computeDisplay,
+        won : false
     }
 
     generatedMot() {
@@ -22,53 +24,61 @@ class App extends Component{
     }
 
 
-    computeDisplay(motActuel, lettreUtilise) {
-        return motActuel.replace(/\w/g, (lettre) => (lettreUtilise.has(lettreUtilise) ? lettre : '_'))
-    }
-
-    restart(){
-        let {motActuel,  nbreEssai, lettreChoisi, lettreUtilise, etatMot}= this.state
+    restart() {
+        let {motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot, won} = this.state
         motActuel = this.generatedMot()
         nbreEssai = 0
         lettreChoisi = ''
         lettreUtilise = new Set()
         etatMot = this.computeDisplay
-        this.setState({motActuel, nbreEssai ,lettreChoisi, lettreUtilise, etatMot})
+        won = false
+        this.setState({motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot, won})
     }
 
-    handleLettre (lettre) {
-        let {motActuel , nbreEssai, lettreChoisi, lettreUtilise, etatMot} = this.state
+    handleLettre(lettre) {
+        let {motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot} = this.state
         lettreChoisi = lettre
-        nbreEssai ++
+        nbreEssai++
         lettreUtilise.add(lettreChoisi)
-        etatMot = this.computeDisplay(motActuel, lettreUtilise)
-        this.setState({motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot})
+        etatMot = computeDisplay(motActuel, lettreUtilise)
+        const won = !etatMot.includes('_')
+        this.setState({motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot, won})
     }
 
 
     render() {
-        const { motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot} = this.state
+        const {motActuel, nbreEssai, lettreChoisi, lettreUtilise, etatMot, won} = this.state
         return (
-            <div className="pendu">
-                <button onClick={() => this.restart()}>Recommencer</button>
-                <p>{lettreUtilise}</p>
-                <NbreEssais nbreEssai={nbreEssai}/>
+            <div className={`pendu ${(won && 'won') || ''}`}>
                 <p className="display">{etatMot}</p>
-                <p>{motActuel}</p>
-                <div>Lettre cliqué : {lettreChoisi}</div>
                 <p className="lettres">
-                    {LETTRES.map((lettre) => (
-                        <button
-                            key={lettre}
-                            onClick={() => this.handleLettre(lettre)}
-                        >
-                            {lettre}
-                        </button>
-                    ))}
+                    {won ? (
+                        <button className="replay" onClick={() => this.restart()}>Recommencer</button>
+                    ) : (
+                        LETTRES.map((lettre) => (
+                            <button
+                                key={lettre}
+                                onClick={() => this.handleLettre(lettre)}
+                            >
+                                {lettre}
+                            </button>
+                        ))
+                    )}
                 </p>
+                <NbreEssais nbreEssai={nbreEssai}/>
+                <div> Lettre utilisée : <p>{lettreUtilise}</p></div>
+                <div>Lettre cliqué : {lettreChoisi}</div>
             </div>
         )
 
     }
+}
+function computeDisplay(phrase, usedLetters) {
+
+    return phrase.replace(
+        /\w/g,
+
+        (letter) => (usedLetters.has(letter) ? letter : '_')
+    )
 }
 export default App;
